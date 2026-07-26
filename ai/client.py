cdf3ab436261ee18067bac18febff8ebeb8dbedc
alan.urga@gmail.com <kyuwe@MacBook-Air-de-Alan.local>
2026-07-25 20:09:25 -0600

    creacion del agente de IA para procesar la informacion

--- ai/client.py
+++ ai/client.py
@@ -1,9 +1,31 @@
+from openai import OpenAI
+import json
+
 class AIClient:
-    def __init__(self, prompt : str):
-        self.prompt = prompt
+    def __init__(self, api_key: str):
+        self.client = OpenAI(
+            api_key=api_key,
+            base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+        )
 
-    def analizar(self) -> str:
+    def analizar(self, prompt : str) -> str:
+        response = self.client.chat.completions.create(
+            model="gemini-flash-latest",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )
 
-        ...
+        # --- LOG PARA VER EL OBJETO COMPLETO ---
+        print("\n=== [DEBUG] RESPUESTA COMPLETA DEL LLM ===")
+        # .model_dump_json() convierte el objeto de la API en un texto JSON limpio
+        print(json.dumps(json.loads(response.model_dump_json()), indent=4, ensure_ascii=False))
+        print("==========================================\n")
+        # ----------------------------------------
 
-        return respuesta
+        respuesta = response.choices[0].message.content
+        respuesta_json = json.loads(respuesta)
+        return respuesta_json
--- orchestrator/orchestrator.py
+++ orchestrator/orchestrator.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -6,6 +7,7 @@ load_dotenv()
 from extractors.image_extractor import AgenteOCR
 from extractors.markitdown_extractor import AgenteDocumentos
 from ai.prompt_manager import PromptManager
+from ai.client import AIClient
 
 class DocumentOrchestrator:
     def __init__(self, api_key: str):
@@ -13,6 +15,7 @@ class DocumentOrchestrator:
         self.agente_doc = AgenteDocumentos()
         self.agente_ocr = AgenteOCR(api_key=api_key)  
         self.prompt_manager = PromptManager()
+        self.ai_client = AIClient(api_key=api_key)
         self.formatos_doc = ('.pdf', '.xlsx', '.xls', '.docx', '.pptx', '.csv')
         self.formatos_img = ('.png', '.jpg', '.jpeg')
 
@@ -37,20 +40,26 @@ class DocumentOrchestrator:
             return f"[Orquestador] -> Error: Formato '{ext}' no soportado."
 
         prompt = self.prompt_manager.construir_prompt(texto_extraido)
-        return prompt
+        respuesta = self.ai_client.analizar(prompt)
+        return respuesta
 
 #main para pruebas
 if __name__ == "__main__":
     API_KEY = os.getenv("GOOGLE_API_KEY")
     orquestador = DocumentOrchestrator(api_key=API_KEY)
 
-    archivo = r"data/input/archivoprueba1.pdf"
-    #archivo =  r"data/input/imagenprueba1.png"
+    #archivo = r"data/input/archivoprueba1.pdf"
+    archivo =  r"data/input/imagenprueba1.png"
 
     resultado = orquestador.enrutar_y_procesar(archivo)
+    
 
     #guardar el resultado
     outputfile = "data/output/"
-    with open(outputfile + "promptFinal.md", "w", encoding="utf-8") as save:
-        save.write(resultado)
+    #with open(outputfile + "promptFinal.md", "w", encoding="utf-8") as save:
+    #with open(outputfile + "RespuestaIA.md", "w", encoding="utf-8") as save:
+    #    save.write(resultado)
+    with open(outputfile + "RespuestaIA.json", "w", encoding="utf-8") as save:
+        json.dump(resultado, save, indent=4, ensure_ascii=False)
+
     #print(resultado)
